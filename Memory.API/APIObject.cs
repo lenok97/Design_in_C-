7db9cab3001d7477a32578ed1e2aee1e865c6ef1
lenok97 <iamhappluck@gmail.com>
2021-10-03 00:34:06 +0500

    Memory.API

---
+++ Memory.API/APIObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        private int id;
+        private bool isDisposed = false;
+
+        public APIObject(int id)
+        {
+            this.id = id;
+            MagicAPI.Allocate(id);
+        }
+
+        ~APIObject()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(bool calledFromDisposeMethod)
+        {
+            if (!isDisposed)
+            {
+                MagicAPI.Free(id);
+                isDisposed = true;
+            }
+        }
+    }
+}
